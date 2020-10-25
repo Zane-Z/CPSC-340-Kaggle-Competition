a5800a01828f9d8beaf6477b41c5b7e0acaaa834
kemiao <kmiao@mitacs.ca>
2020-10-25 12:24:39 -0700

    Time series model implementation and usage

--- Code/utils2.py
+++ Code/utils2.py
@@ -4,12 +4,34 @@ Created on Sun Oct 25 00:24:00 2020
 
 @author: kmiao
 """
+import numpy as np                              # this comes with Anaconda
+import pandas as pd                             # this comes with Anaconda
 
 #--------------------------Kay Part----------------------------
-y_can.shape
-new_max=np.zeros((275,5))
-#new_df=pd.DataFrame(columns=("1", "2", "3", "4", "5"))
-for i in range(0, 275):
-    new_max[i]=y_can[i:(i+5), 0]
-
-new_max_one = np.hstack((np.ones((275, 1)), new_max))
+class TimeSeries:
+    def __init__(self):
+        pass
+    
+    def matrix_to_tseries(self, X, window_length=5, preapp_one=True): 
+        x_shape = X.shape
+        num_row = x_shape[0]
+        num_col = x_shape[1]
+        new_dim = num_row-window_length
+    
+        if (num_col==1):
+            new_max=np.zeros((new_dim, window_length))
+    
+            for i in range(0, new_dim):
+                new_max[i]=X[i:(i+window_length), 0]
+            if (preapp_one == True):
+                new_max = np.hstack((np.ones((new_dim, 1)), new_max))
+                
+            self.new_y = X[window_length:,]
+            self.new_max = new_max
+            
+            return "Completed"
+            
+        elif (num_col>1):
+            return "invalid input"
+        else:
+            return "invalid input"
